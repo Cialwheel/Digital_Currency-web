30380044669f49c2a967af7700160731b81f4e08
chen.lin <chen.lin@chenllins-MacBook-Pro.local>
2021-07-01 10:24:47 +0800

    add bootstrap flash

--- app/controllers/admin/coins_controller.rb
+++ app/controllers/admin/coins_controller.rb
@@ -1,2 +1,17 @@
 class Admin::CoinsController < ApplicationController
+  def import
+    Coin.import(params[:file])
+    redirect_to coins_url
+  end
+
+  def index
+    @coins = Coin.all
+  end
+
+  def destroy
+    @coins = Coin.all
+    @coins.destroy_all
+    flash[:warning] = "cleaned"
+    redirect_to coins_path
+  end
 end
