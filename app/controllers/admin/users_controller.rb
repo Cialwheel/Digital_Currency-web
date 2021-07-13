d8f967b0c9b625153fb0bff09d22a550ed57814d
chen.lin <chen.lin@192.168.0.105>
2021-07-13 11:16:27 +0800

    form member? finish

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,36 @@ class Admin::UsersController < ApplicationController
   layout "admin"
   def index
     @users = User.all
+    @c = @users.count
   end
 
+  def show
+    @users = User.all
+    @n=0
+    @users.each do |user|
+      if user.is_member == true
+        @n = @n + 1
+      end
+    end
+    @nprime = @users.count - @n
+
+    @con = [@n,@nprime]
+    colors = ['rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+                ]
+    @data1 = {
+        labels: ["会员","非会员"],
+        datasets: [{
+          label: "会员参考",
+          data: @con.map{|t|t.to_i},
+          backgroundColor: colors,
+          borderWidth: 1
+        }]
+    }
+  end
 
 end
